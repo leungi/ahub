ed00f999b60ebd6e20bed44c424f1c8193b47449
Martin Hanewald <martin.hanewald@gmail.com>
2018-11-15 13:36:24 +0100

    Further tries

--- modules/reactgui/src/index.js
+++ modules/reactgui/src/index.js
@@ -1,21 +1,74 @@
 
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+//import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
 // ========================================
-
-
 
 class AhubGUI extends React.Component {
   render(){
     return(
-      <div className="w3-row">
-        <NodeBox name="node1"/>
-        <NodeBox name="node2"/>
+      <div className="wrapper">
+        <nav id="sidebar">
+        <div className="sidebar-header">
+          <h3>Bootstrap Sidebar</h3>
       </div>
+
+      <ul className="list-unstyled components">
+          <p>Dummy Heading</p>
+          <li className="active">
+              <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Home</a>
+              <ul className="collapse list-unstyled" id="homeSubmenu">
+                  <li>
+                      <a href="#">Home 1</a>
+                  </li>
+                  <li>
+                      <a href="#">Home 2</a>
+                  </li>
+                  <li>
+                      <a href="#">Home 3</a>
+                  </li>
+              </ul>
+          </li>
+          <li>
+              <a href="#">About</a>
+          </li>
+          <li>
+              <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Pages</a>
+              <ul className="collapse list-unstyled" id="pageSubmenu">
+                  <li>
+                      <a href="#">Page 1</a>
+                  </li>
+                  <li>
+                      <a href="#">Page 2</a>
+                  </li>
+                  <li>
+                      <a href="#">Page 3</a>
+                  </li>
+              </ul>
+          </li>
+          <li>
+              <a href="#">Portfolio</a>
+          </li>
+          <li>
+              <a href="#">Contact</a>
+          </li>
+      </ul>
+
+        </nav>
+        <nav id = "content">
+          <div className="w3-row">
+            <NodeBox name="node1"/>
+            <NodeBox name="node2"/>
+          </div>
+          </nav>
+        </div>
     )
   }
 }
@@ -31,39 +84,44 @@ class NodeBox extends React.Component {
 
   render(){
     return(
-      <div className="nodebox w3-col m4 l3">
-        <div className="boxheader">
-          {this.props.name}
-        </div>
-        <div className="boxbody">
-          <button
-            className="endpointbutton"
-            onClick={() => this.setState({response:
-                triggerEndpoint(this.props.name, "batch")}
-              )}>
-            GET
-          </button>
-          <div className="response">
-            {this.state.response}
+
+          <div className="nodebox w3-col">
+            <div className="boxheader">
+              {this.props.name}
+            </div>
+            <div className="boxbody">
+              <button
+                className="endpointbutton"
+                onClick={() => this.triggerEndpoint(this.props.name, "batch")}>
+                GET
+              </button>
+              <div className="response">
+                {this.state.response}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+
     )
   }
+
+  triggerEndpoint(nodename, funcname) {
+      var url = "https://ahub@ilikebigwhales:ahub.westeurope.cloudapp.azure.com/" +
+        nodename + "/" + funcname
+      $.getJSON(url, function(data) {this.setresponse(data)});
+      return(
+        "TEST"// instead run GET Request on above URL
+      )
+  }
+
+  setresponse(data){
+    this.setState({response: data})
+  }
+
 }
 // ========================================
 
 ReactDOM.render(
+  //<ThemeSwitcher/>,
   <AhubGUI />,
   document.getElementById('root')
 );
-
-
-function triggerEndpoint(nodename, funcname) {
-    var url = "http://ahub.westeurope.cloudapp.azure.com:8000/" +
-      nodename + "/" + funcname
-    return(
-      url
-      // instead run GET Request on above URL
-    )
-}
