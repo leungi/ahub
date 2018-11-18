e134a1e3db4d638c18aaab62a9d252a66bfbdfaa
Milan Wanielik <wanielik@gmail.com>
2018-11-18 20:08:37 +0100

    Add grommet; rework UI

--- modules/reactgui/src/components/AhubGUI.js
+++ modules/reactgui/src/components/AhubGUI.js
@@ -1,37 +1,126 @@
 import React from 'react';
-import {get} from '../modules/fetch';
+import {
+    Grommet,
+    Grid,
+    Box,
+    Image,
+} from 'grommet'
+import {
+    Router,
+} from '@reach/router';
+import { get } from '../modules/fetch';
+import MainNavLink from './MainNavLink';
 import NodeBox from './NodeBox';
 
 const API_ENDPOINT = 'http://ahub.westeurope.cloudapp.azure.com:8000/';
 //const API_ENDPOINT = 'localhost:8000';
 
+const theme = {
+    global: {
+        colors: {
+            brand: '#003E81',
+            'accent-1': '#009EDA',
+            'neutral-1': '#000',
+        },
+        font: {
+            family: 'Roboto',
+            size: '14px',
+            height: '20px',
+        },
+    },
+};
+
 export default class AhubGUI extends React.Component {
-    constructor (props) {
+    constructor(props) {
         super(props);
         this.state = {
             nodes: []
         };
     }
 
-    componentDidMount () {
+    componentDidMount() {
         get(`${API_ENDPOINT}boss/get_services`)
-            .then(response => this.setState({nodes: response.apis}))
+            .then(response => this.setState({ nodes: response.apis }))
             .catch(err => {
                 console.warn(err);
                 // prefill nodes for testing purposes
-                this.setState({nodes: ['node1', 'node2']});
+                this.setState({ nodes: ['node1', 'node2'] });
             });
     }
 
-    render(){
-        return(
-            <div className="w3-row">
-                {
-                    this.state.nodes
-                    ? this.state.nodes.map(node => <NodeBox key={`node-${node}`} name={node} />)
-                    : <p>Loading nodes...</p>
-                }
-            </div>
+    render() {
+        return (
+            <Grommet 
+                theme={theme} 
+                full
+            >
+                    <Grid
+                        fill
+                        areas={[
+                            { name: 'nav', start: [0, 0], end: [0, 0] },
+                            { name: 'main', start: [1, 0], end: [1, 0] }
+                        ]}
+                        columns={['small', 'flex']}
+                        rows={['flex']}
+                        gap='none'
+                    >
+                        <Box 
+                            gridArea='nav' 
+                            background='brand'
+                            pad='medium'
+                        >
+                            <Box>
+                                <Image 
+                                    src='https://qunis.de/wordpress-qunis/wp-content/themes/qunis-theme-2016/images/interface/qunis-big-data-logo-w2x.png'
+                                />
+                            </Box>
+                            <Box
+                                tag='nav'
+                                direction='column'
+                                margin={{
+                                    vertical: 'large'
+                                }}
+                            >
+                                <MainNavLink to='/'>Home</MainNavLink>
+                                {
+                                    this.state.nodes
+                                    && this.state.nodes.map(node => (
+                                        <MainNavLink 
+                                            key={`navlink-${node}`}
+                                            to={node}
+                                        >
+                                            {node}
+                                        </MainNavLink>
+                                    ))
+                                }
+                            </Box>
+                        </Box>
+                        <Box 
+                            gridArea='main' 
+                            background='brand' 
+                        >
+                            <Router>
+                                {
+                                    this.state.nodes
+                                    && this.state.nodes.map(node => (
+                                        <NodeBox 
+                                            key={`nodebox-${node}`}
+                                            path={node} 
+                                            name={node} 
+                                        />
+                                    ))
+                                }
+                            </Router>
+                        </Box>
+                    </Grid>
+            </Grommet>
         )
     }
 }
+ /*
+ {
+    this.state.nodes
+        ? this.state.nodes.map(node => <NodeBox key={`node-${node}`} name={node} />)
+        : <p>Loading nodes...</p>
+} 
+*/
